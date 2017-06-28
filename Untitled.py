0ecf5c97681268f569d0e99d8e473fa610ef5b24
Shubham Vadhera <Shubham.Vadhera@elliemae.com>
2017-06-28 14:12:25 -0700

    new

--- Untitled.py
+++ Untitled.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[44]:
+# In[61]:
 
 import csv
 import os
@@ -10,13 +10,16 @@ source_dir = 'C:\\Users\\svadhera\\Documents\\inputfiles\\'
 target_dir = source_dir + '\\processed\\'
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
-
+mainlist=[]
+head=True
 for root,dirs,files in os.walk(source_dir):
     for fil in files:
         if not fil.endswith(".txt"):
             continue
         inputfile = os.path.join(source_dir,fil)
         input_set = []
+        newinp = []
+        colinput = []
         with open(inputfile) as inputs:
             for line in inputs:
                 currLine = line.strip()
@@ -32,15 +35,24 @@ for root,dirs,files in os.walk(source_dir):
             input_set.append(currLine)
             if 'Test Result' in currLine:
                 break
-        print input_set
-        
-        with open(target_dir+fil+'_out.csv','wb') as csv_file:
-                writer = csv.writer(csv_file, delimiter=',')
-                for inp in input_set:
-                    lis = inp.split(':',1)
-                    lis = [x.strip() for x in lis]
-                    writer.writerow(lis)
-    
+        #print input_set
+        for inp in input_set:
+            if head:
+                col = inp.split(':',1)[0]
+                colinput.append(col.strip())
+            col = inp.split(':',1)[1]
+            newinp.append(col.strip())
+            #writer.writerow(newinp)
+            #print(newinp)
+        if head:
+            mainlist.append(colinput)
+            head=False
+        mainlist.append(newinp)
+#print(zip(*mainlist))
+with open(target_dir+'out.csv','wb') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    for x in mainlist:
+        writer.writerow(x)
 
 
 # In[ ]:
